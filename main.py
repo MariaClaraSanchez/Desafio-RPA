72a1727c5633605b5e05873dcbb12f67af4fb962
bruno-cabralz <bruno_josecabral@hotmail.com>
2022-01-18 13:57:24 -0300

    add planilha e coleta dos titulos das vagas

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+from openpyxl import Workbook
+from datetime import date
+
+# Cria uma pasta de trabalho para conter a planilha
+arquivo_excel = Workbook()
+
+# Ativa a guia que já vem por padrão no Workbook
+sheetVagas = arquivo_excel.active
+
+# Pega a data de hoje para nomear a guia da planilha
+today = date.today()
+
+# Renomenado o título da guia para a data de hoje
+sheetVagas.title = format(today)
+
+# Escrevendo no arquivo o Nome/Local e Descrição da vaga
+sheetVagas['A1'] = "Nome"
+sheetVagas['B1'] = "Local"
+sheetVagas['C1'] = "Descrição"
+
+# Salvando o arquivo
+arquivo_excel.save('Vagas do Dia.xlsx')
 
 
 chrome_options = webdriver.ChromeOptions()
@@ -21,4 +43,8 @@ time.sleep(2)
 for vaga in vagas:
     print(vaga.accessible_name)
 time.sleep(5)
+
+locais = driver.find_elements(By.XPATH,)
+
+
 driver.close()
