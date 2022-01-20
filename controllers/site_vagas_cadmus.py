b6dff020cea934f47f2b93c023440223b94e89c1
Maria Sanchez <clarasanchez2398@gmail.com>
2022-01-20 14:50:25 -0300

    Codigo sendo refatorado, contém alguns erros

---
+++ controllers/site_vagas_cadmus.py
@@ -0,0 +1,62 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+import re
+
+class SiteCadmus : 
+    def __init__(self) -> None:
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--lang=pt-BR')
+        chrome_options.add_argument('disable-infobars')
+        chrome_options.add_argument('--log-level=3')
+
+        self.driver =  webdriver.Chrome(options=chrome_options)
+
+    def acessar_vagas(self) -> None:
+        self.driver.get('https://cadmus.com.br/vagas-tecnologia/')
+        time.sleep(5)
+
+    def pegar_vagas(self) -> list:
+        vagas = self.driver.find_elements(By.XPATH, '//div[@class="box"]')
+        time.sleep(2)
+
+        self.driver.execute_script("window.scrollTo(0,1000);")
+        time.sleep(2)
+
+        dados_vagas = []
+
+        for vaga in vagas:
+
+            titulo_el = vaga.find_element(By.TAG_NAME, 'h3')
+            local_el = vaga.find_element(By.CLASS_NAME, 'local')
+            titulo = titulo_el.text
+            local = local_el.text
+
+
+            # Titulo é chave
+            dados_vagas.append({titulo: {
+                'local': local,
+                'descricao': None
+            }})
+
+        return dados_vagas
+
+    def pegar_descricao_vagas(self, dados_vagas:list ) -> list:
+        for vaga in dados_vagas:
+            self.driver.execute_script("window.scrollTo(0,1000);")
+            time.sleep(1)
+            btn_descricao = '//div[@class="box"]/h3[contains(text(), "{}")]/../p/a'.format(
+                list(vaga.keys())[0])
+            link = self.driver.find_element(
+                By.XPATH, btn_descricao)
+            link.send_keys(Keys.RETURN)
+            descricao = self.driver.find_element(
+                By.XPATH, '//div[@class="box z-depth-1"]/p')
+
+            dados_vagas.append({
+                'descricao': descricao
+            })
+            self.driver.back()
+
+        return dados_vagas
